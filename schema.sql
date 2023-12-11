7456eb55305bc668e7a6f0e1fc084abadca220db
alannesanni <sanni.alanne@helsinki.fi>
2023-12-11 17:57:45 +0200

    Add references to schema

--- schema.sql
+++ schema.sql
@@ -4,8 +4,8 @@ CREATE TABLE ingredients (id SERIAL PRIMARY KEY, name TEXT, place TEXT);
 
 CREATE TABLE recipes (id SERIAL PRIMARY KEY, name TEXT, ingredient_ids TEXT, instructions TEXT);
 
-CREATE TABLE selected_ingredients (id INTEGER PRIMARY KEY, selected TEXT);
+CREATE TABLE selected_ingredients (id INTEGER PRIMARY KEY REFERENCES users(id), selected TEXT);
 
-CREATE TABLE liked_recipes (id SERIAL PRIMARY KEY, user_id INTEGER, recipe_id INTEGER)
+CREATE TABLE liked_recipes (id SERIAL PRIMARY KEY, user_id INTEGER REFERENCES users(id), recipe_id INTEGER REFERENCES recipes(id))
 
 
