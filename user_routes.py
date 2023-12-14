cc7f56c492b2e3e01ab0552f72e11622d58f78ce
alannesanni <sanni.alanne@helsinki.fi>
2023-12-14 22:55:47 +0200

    Fix pylint issues

--- user_routes.py
+++ user_routes.py
@@ -7,7 +7,8 @@ import methods.recipe_methods as recipe_methods
 @app.route("/home")
 def home():
     if "id" not in session:
-        return render_template("error.html", error_message="You need to be logged in to access this page.")
+        return render_template("error.html",
+                    error_message="You need to be logged in to access this page.")
     selected_ing = ing_methods.get_selected_ingredients("name")
     ing_names_fridge = selected_ing[0]
     ing_names_pantry = selected_ing[1]
@@ -15,16 +16,19 @@ def home():
     liked_rec = like_methods.get_liked_recipes()
     liked_count = like_methods.user_liked_recipes_count()
     if recipes:
-        return render_template("home.html", ing_names_fridge=ing_names_fridge, ing_names_pantry=ing_names_pantry, 
-                               recipes=recipes, liked_rec=liked_rec, liked_count=liked_count)
-    return render_template("home.html", ing_names_fridge=ing_names_fridge, ing_names_pantry=ing_names_pantry, 
-                           recipes=None, liked_rec=liked_rec, liked_count=liked_count)
+        return render_template("home.html", ing_names_fridge=ing_names_fridge,
+                               ing_names_pantry=ing_names_pantry, recipes=recipes,
+                               liked_rec=liked_rec, liked_count=liked_count)
+    return render_template("home.html", ing_names_fridge=ing_names_fridge,
+                           ing_names_pantry=ing_names_pantry, recipes=None,
+                           liked_rec=liked_rec, liked_count=liked_count)
 
 
 @app.route("/update", methods=["GET", "POST"])
 def update():
     if "id" not in session:
-        return render_template("error.html", error_message="You need to be logged in to access this page.")
+        return render_template("error.html",
+                               error_message="You need to be logged in to access this page.")
     if request.method == "POST":
         if session["csrf_token"] != request.form["csrf_token"]:
             return render_template("error.html")
@@ -40,7 +44,8 @@ def update():
 @app.route("/recipe/<recipe_name>")
 def recipe(recipe_name):
     if "id" not in session:
-        return render_template("error.html", error_message="You need to be logged in to access this page.")
+        return render_template("error.html",
+                               error_message="You need to be logged in to access this page.")
     recipe_tuple = recipe_methods.get_recipe(recipe_name)
     if not recipe_tuple:
         return render_template("error.html", error_message="We don't have this recipe.")
@@ -48,7 +53,7 @@ def recipe(recipe_name):
     length_ing = len(recipe_tuple[1])
     likes = like_methods.recipe_likes(recipe_name)
     return render_template("recipe.html", recipe_name=recipe_tuple[0],
-                           recipe_ingredients=recipe_tuple[1], recipe_instructions=recipe_tuple[2], 
+                           recipe_ingredients=recipe_tuple[1], recipe_instructions=recipe_tuple[2],
                            length_ing=length_ing, is_liked=is_liked, likes=likes)
 
 
